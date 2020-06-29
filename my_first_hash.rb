7da54023e1d7698c90a97d89d160229a8af56307
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-29 00:56:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,8 +25,8 @@ def adding
                   "oil paintings" => 3
                  }
                   shipping_manifest["musket"] = 2
-                   shipping_manifest["gun powder"] = 4
+                  shipping_manifest["gun powder"] = 4
 
-shipping_manifest
+  shipping_manifest
 
 end
